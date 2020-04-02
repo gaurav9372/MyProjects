4ec98cb5c5c110daa6db2ac468f64194e9febe83
Shreyansh Gaurav <gauravguptasggs@gmail.com>
2020-04-02 14:54:51 +0530

    j

---
+++ Neumorphism/button.js
@@ -0,0 +1,8 @@
+function clicked() {
+    
+var pressed = document.getElementById('button');
+    
+pressed.style.boxShadow =
+    '3px 3px 8px 0 rgba(0,0,0,0.45) inset , -2px -2px 6px 0 rgba(255,255,255,0.75) inset';
+pressed.style.fontSize='1.90em'; 
+}
