14a4af8ecd96eef8c39cad1838bbfe5f60679adf
Shreyansh Gaurav <gauravguptasggs@gmail.com>
2020-04-17 22:10:48 +0530

    j

---
+++ CSS/AOS/xyz.js
@@ -0,0 +1,12 @@
+AOS.init({
+  duration: 1200,
+})
+//AOS.init();
+
+//AOS.init({
+//   easing: 'ease-out-back',
+//   duration: 800,
+//   delay: 300,
+//   once: true,
+//   disable: 'mobile'
+//});
