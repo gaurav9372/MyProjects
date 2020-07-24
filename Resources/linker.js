0c51bdb3df245f06de998fc6da3850b1b6f1601b
Shreyansh Gaurav <gauravguptasggs@gmail.com>
2020-07-24 19:19:39 +0530

    yo

--- Resources/linker.js
+++ Resources/linker.js
@@ -1,12 +1,28 @@
 $(document).ready(function(){
      
+
+    
 $('li').each(function(index){
     
-$(this).hide().fadeIn(index*200);
+$(this).hide().fadeIn(index*100);
+$(this).prepend("<div></div>")
+    
+
     
 var folderName = $(this).text().trim();
-var location ="CSS/"+folderName+ "/index.html";
+var folderName2 = $(this).text();
+var location =folderName+ "/index.html";
+var sspath = "url("+'"'+folderName+"/Screenshot.png"+'"'+")";
+      
 
+$(this).find('div').css({
+"background": sspath,
+    "height": "90%",
+    "width": "100%",
+    "background-size": "75%",
+    "background-repeat": "no-repeat",
+    "background-position": "50% 50%",
+}); 
     
 $(this).find('a').attr('href',location);
     
