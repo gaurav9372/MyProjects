d769921fc149fd3316a2d179ee3cb8477a6b54ec
Shreyansh Gaurav <gauravguptasggs@gmail.com>
2020-07-26 14:11:07 +0530

    Memorie Initiated

---
+++ Works/Memorie/resources/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+     
+
+$('.memo').click(function(){
+    
+$(this).css({
+    "position":"absolute",
+    "top":"0px",
+    "left":"0px",
+    "height":"100%",
+    "width":"100%",
+    "display":"flex",
+    "border-radius":"0px",
+    "margin": "0px",
+    "transition": "200ms all"
+});
+                 
+});
+    
+    
+});
