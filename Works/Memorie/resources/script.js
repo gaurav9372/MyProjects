309e13de3d20e6eb9d5e0744b410b138ca426414
Shreyansh Gaurav <gauravguptasggs@gmail.com>
2020-07-26 19:41:04 +0530

    yo

--- Works/Memorie/resources/script.js
+++ Works/Memorie/resources/script.js
@@ -1,21 +1,57 @@
 $(document).ready(function(){
-     
+   
 
 $('.memo').click(function(){
     
-$(this).css({
-    "position":"absolute",
-    "top":"0px",
-    "left":"0px",
-    "height":"100%",
-    "width":"100%",
-    "display":"flex",
-    "border-radius":"0px",
-    "margin": "0px",
-    "transition": "200ms all"
+    
+$("#priNavbar").hide();
+$("#secNavbar").hide();
+$("#newMemo").hide();
+    
+$(this).addClass("expand");
+    
+$(".memoNavbar").removeClass("hide").addClass("show"); 
+
+    
+    
+
 });
-                 
-});
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
 });
+
+
+
+//{
+//    "position":"absolute",
+//    "top":"0px",
+//    "left":"0px",
+//    "height":"100%",
+//    "width":"100%",
+//    "display":"flex",
+//    "border-radius":"0px",
+//    "margin": "0px",
+//    "background-color": "#fff",
+//    "border":"0px",
+//    "flex-direction":" column",
+//    "padding": "20px",
+//    "color": "#000",
+//    "margin-top": "0px",
+//    "cursor":"default"
+//}
